448a3eb6ee9b3f9676b6959018d4bf089e661181
CraftedShine <fynnschotten@gmail.com>
2024-11-23 23:34:10 +0100

    .gitignore

--- scripts/TableGenerator/TableGenerator/main.py
+++ scripts/TableGenerator/TableGenerator/main.py
@@ -1,6 +1,15 @@
-from argparse import ArgumentParser
+import argparse
 
-parser = ArgumentParser(prog='TableGenerator', description='Builds Tables & API Interfaces for them.')
-general = parser.add_mutually_exclusive_group()
+parser = argparse.ArgumentParser(prog='TableGenerator', description='Builds Tables & API Interfaces for them.')
+general = parser.add_argument_group('general')
 general.add_argument('-c', '--create', help='Creates a new Database')
 general.add_argument('-d', '--delete', help='Deletes Database')
+
+args = parser.parse_args()
+
+def main():
+    print("That worked")
+    print(args)
+
+if __name__ == "__main__":
+    main()
--- scripts/TableGenerator/setup.py
+++ scripts/TableGenerator/setup.py
@@ -2,6 +2,13 @@ from setuptools import setup
 from TableGenerator import __version__
 
 setup(
-    name='tablegenerator',
-    version=__version__
+   name='tablegenerator',
+   version=__version__,
+   description='A useful module',
+   author='Fynn Schotten',
+   author_email='fynnschotten@gmail.com',
+   packages=['tablegenerator'],
+   entry_points={
+      'console_scripts': ['tablegenerator=main:main'],
+   }
 )
